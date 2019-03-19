96851b073b0f7444eab06a6f7e9d2e8027761529
zd3F07 <zd3f07@gmail.com>
2019-03-19 15:36:36 +0900

    page-member.php

---
+++ page-member.php
@@ -0,0 +1,125 @@
+<?php get_header(); ?>
+  <section class="catch"></section>
+
+  <main>
+    <div class="container">
+      <section class="block">
+          <div class="block_body">
+            <h2 class="block_body_title">社員紹介</h2>
+            <div class="seiretsu">
+              <div class="row">
+              <div class="col-lg-12 col-md-4">
+                <div class="memberImg">
+                  <img src="images/user.png" alt="yamadataro">
+                </div>
+                <h3>山田太郎</h3>
+                <table class="profile">
+                  <tr>
+                    <th>趣味</th>
+                    <td>映画鑑賞</td>
+                  </tr>
+                  <tr>
+                    <th>好きな食べ物</th>
+                    <td>そば</td>
+                  </tr>
+                </table>
+              </div><!-- /.col-lg-12 -->
+          </div><!-- /.row -->
+          <div class="row">
+              <div class="col-lg-12 col-md-4">
+                <div class="memberImg">
+                  <img src="images/user.png" alt="yamadataro">
+                </div>
+                <h3>山田太郎</h3>
+                <table class="profile">
+                  <tr>
+                    <th>趣味</th>
+                    <td>映画鑑賞</td>
+                  </tr>
+                  <tr>
+                    <th>好きな食べ物</th>
+                    <td>そば</td>
+                  </tr>
+                </table>
+              </div><!-- /.col-lg-12 -->
+          </div><!-- /.row -->
+          <div class="row">
+              <div class="col-lg-12 col-md-4">
+                <div class="memberImg">
+                  <img src="images/user.png" alt="yamadataro">
+                </div>
+                <h3>山田太郎</h3>
+                <table class="profile">
+                  <tr>
+                    <th>趣味</th>
+                    <td>映画鑑賞</td>
+                  </tr>
+                  <tr>
+                    <th>好きな食べ物</th>
+                    <td>そば</td>
+                  </tr>
+                </table>
+              </div><!-- /.col-lg-12 -->
+          </div><!-- /.row -->
+          </div><!-- /.seiretu -->
+          <div class="seiretsu">
+              <div class="row">
+              <div class="col-lg-12 col-md-4">
+                <div class="memberImg">
+                  <img src="images/user.png" alt="yamadataro">
+                </div>
+                <h3>山田太郎</h3>
+                <table class="profile">
+                  <tr>
+                    <th>趣味</th>
+                    <td>映画鑑賞</td>
+                  </tr>
+                  <tr>
+                    <th>好きな食べ物</th>
+                    <td>そば</td>
+                  </tr>
+                </table>
+              </div><!-- /.col-lg-12 -->
+          </div><!-- /.row -->
+          <div class="row">
+              <div class="col-lg-12 col-md-4">
+                <div class="memberImg">
+                  <img src="images/user.png" alt="yamadataro">
+                </div>
+                <h3>山田太郎</h3>
+                <table class="profile">
+                  <tr>
+                    <th>趣味</th>
+                    <td>映画鑑賞</td>
+                  </tr>
+                  <tr>
+                    <th>好きな食べ物</th>
+                    <td>そば</td>
+                  </tr>
+                </table>
+              </div><!-- /.col-lg-12 -->
+          </div><!-- /.row -->
+          <div class="row">
+              <div class="col-lg-12 col-md-4">
+                <div class="memberImg">
+                  <img src="images/user.png" alt="yamadataro">
+                </div>
+                <h3>山田太郎</h3>
+                <table class="profile">
+                  <tr>
+                    <th>趣味</th>
+                    <td>映画鑑賞</td>
+                  </tr>
+                  <tr>
+                    <th>好きな食べ物</th>
+                    <td>そば</td>
+                  </tr>
+                </table>
+              </div><!-- /.col-lg-12 -->
+          </div><!-- /.row -->
+          </div><!-- /.seiretu -->
+        </div><!-- /.block_body -->
+      </section>
+    </div>
+  </main>
+<?php get_footer(); ?>
